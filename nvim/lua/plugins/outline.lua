9a55a65757e2975215468ce7e2f71993affc1b2d
rio <rio@gmail.com>
2026-01-30 10:47:17 +0000

    add outline plugins

--- nvim/lua/plugins/outline.lua
+++ nvim/lua/plugins/outline.lua
@@ -2,11 +2,47 @@ return {
 	"hedyhli/outline.nvim",
 	lazy = true,
 	cmd = { "Outline", "OutlineOpen" },
-	keys = { -- Example mapping to toggle outline
-		{ "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+	keys = {
+		{ "<leader>o", "<cmd>Outline<cr>", desc = "Toggle Outline" },
 	},
 	opts = {
-		-- Your setup opts here
+		outline_window = {
+			position = "right",
+			width = 25,
+		},
+		symbols = {
+			icons = {
+				File = { icon = "󭊔", hl = "Identifier" },
+				Module = { icon = "󰏗", hl = "Include" },
+				Namespace = { icon = "󰅪", hl = "Include" },
+				Package = { icon = "󰏗", hl = "Include" },
+				Class = { icon = "󭵶", hl = "Type" },
+				Method = { icon = "󰆧", hl = "Function" },
+				Property = { icon = "󰜢", hl = "Identifier" },
+				Field = { icon = "󰜢", hl = "Identifier" },
+				Constructor = { icon = "󰆧", hl = "Special" },
+				Interface = { icon = "󭵶", hl = "Type" },
+				Function = { icon = "󰊕", hl = "Function" },
+				Variable = { icon = "󰀫", hl = "Constant" },
+				Constant = { icon = "󰏿", hl = "Constant" },
+				String = { icon = "󰀬", hl = "String" },
+				Number = { icon = "󰎠", hl = "Number" },
+				Boolean = { icon = "󰨙", hl = "Boolean" },
+				Array = { icon = "󱡠", hl = "Constant" },
+				Object = { icon = "󭵶", hl = "Type" },
+				Key = { icon = "󰌋", hl = "Type" },
+				Null = { icon = "󰟢", hl = "Type" },
+				EnumMember = { icon = "󰒻", hl = "Identifier" },
+				Struct = { icon = "󭵶", hl = "Type" },
+				Event = { icon = "󱐋", hl = "Type" },
+				Operator = { icon = "󰆕", hl = "Operator" },
+				TypeParameter = { icon = "󰊄", hl = "Type" },
+			},
+		},
+		keymaps = {
+			up_and_jump = "<up>",
+			down_and_jump = "<down>",
+		},
 	},
 }
 
