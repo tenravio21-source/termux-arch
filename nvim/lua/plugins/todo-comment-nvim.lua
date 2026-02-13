89bb18d0dd587dd3591199e3a0aa65c3dfaba4f5
rio <rio@gmail.com>
2026-02-13 12:03:19 +0530

    todo-comments

--- nvim/lua/plugins/todo-comment-nvim.lua
+++ nvim/lua/plugins/todo-comment-nvim.lua
@@ -1,7 +1,7 @@
 return {
 	"folke/todo-comments.nvim",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
-	event = { "BufReadPost", "BufNewFile" },
+	cmd = { "TodoTrouble", "TodoTelescope" },
 	opts = {
 		highlight = {
 			keyword = "wide",
