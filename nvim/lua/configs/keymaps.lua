bcc1e5d083a1ac44b2613f3f9b9f00d863a4f37e
rio <rio@gmail.com>
2026-02-23 17:36:16 +0530

    fix which key

--- nvim/lua/configs/keymaps.lua
+++ nvim/lua/configs/keymaps.lua
@@ -1,12 +1,25 @@
-local keymap_configs = {
-	"core",
-	"terminal",
-	"lsp",
-	"git",
-	"search",
-	"ui",
-	"leetcode",
-}
+local wk = require("which-key")
+
+-- 1. Register the Group Names FIRST
+wk.add({
+	{ "<leader>b", group = "Buffer", icon = "󰓩 " },
+	{ "<leader>f", group = "Find/File", icon = "󰈞 " },
+	{ "<leader>g", group = "Git", icon = "󰊢 " },
+	{ "<leader>l", group = "LSP", icon = "󰒋 " },
+	{ "<leader>p", group = "Plugins/Package", icon = "󰏖 " },
+	{ "<leader>q", group = "Session/Quit", icon = "󰈆 " },
+	{ "<leader>u", group = "UI/User", icon = "󰙵 " },
+	{ "<leader>w", group = "Window", icon = "󱂬 " },
+	{ "<leader>h", group = "Help/Helper", icon = "󰞋 " },
+	{ "<leader>s", group = "Search", icon = "󱎸 " },
+	{ "<leader>H", group = "File/Directory", icon = "󰓩 " },
+	{ "<leader>z", group = "Leetcode", icon = "󰒋" },
+	{ "<leader>x", group = "Trouble", icon = " " },
+	{ "<leader>d", group = "Diagnostic", icon = "󱒇 " },
+})
+
+-- 2. NOW load your modular keymaps
+local keymap_configs = { "core", "terminal", "lsp", "git", "search", "ui", "leetcode" }
 
 for _, config_name in ipairs(keymap_configs) do
 	local success, _ = pcall(require, "configs.keymaps-" .. config_name)
