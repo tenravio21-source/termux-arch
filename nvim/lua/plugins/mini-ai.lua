f0c9f123c9d978b7fcdf2c983428dbce7d446e18
rio <rio@gmail.com>
2026-02-25 16:09:59 +0530

    add mini-ai

---
+++ nvim/lua/plugins/mini-ai.lua
@@ -0,0 +1,13 @@
+return {
+	{
+		"echasnovski/mini.ai",
+		version = false,
+		opts = {
+			n_lines = 500, -- How far to look for a text object
+			-- custom_textobjects = {} -- This is where we can link it to Treesitter later
+		},
+		config = function(_, opts)
+			require("mini.ai").setup(opts)
+		end,
+	},
+}
