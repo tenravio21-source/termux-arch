c0212f49e52b01c589ff21d16997c3c7328e9fbf
rio <rio@gmail.com>
2026-02-27 12:25:31 +0530

    replacing telescope with fzf

--- nvim/lua/configs/keymaps-git.lua
+++ nvim/lua/configs/keymaps-git.lua
@@ -1,23 +1,27 @@
 local util = require("utils.keymaps")
 local safe_map = util.safe_map
 
--- 2. WRAP the calls in functions
 -- Search Git Files
 safe_map("n", "<leader>gf", function()
-	require("telescope.builtin").git_files()
-end, "Search Git Files (Telescope)")
+	require("fzf-lua").git_files()
+end, "Search Git Files (Fzf-Lua)")
 
 -- Search Git Branches
 safe_map("n", "<leader>gb", function()
-	require("telescope.builtin").git_branches()
-end, "Search Git Branches (Telescope)")
+	require("fzf-lua").git_branches()
+end, "Search Git Branches (Fzf-Lua)")
 
--- Search Git Commits
+-- Search Git Commits (Project Wide)
 safe_map("n", "<leader>gc", function()
-	require("telescope.builtin").git_commits()
-end, "Search Git Commits (Telescope)")
+	require("fzf-lua").git_commits()
+end, "Search Git Commits (Fzf-Lua)")
 
--- Search Git Commits for Buffer
+-- Search Git Commits for Current Buffer
 safe_map("n", "<leader>gC", function()
-	require("telescope.builtin").git_bcommits()
-end, "Search Git Buffer Commits (Telescope)")
+	require("fzf-lua").git_bcommits()
+end, "Search Git Buffer Commits (Fzf-Lua)")
+
+-- Git Status (Added this because it's great in Fzf-Lua)
+safe_map("n", "<leader>gs", function()
+	require("fzf-lua").git_status()
+end, "Git Status (Fzf-Lua)")
