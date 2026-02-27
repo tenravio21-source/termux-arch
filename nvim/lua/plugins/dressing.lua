fc8fdd5070451033bd8c8504bf0e41d8a79994c1
rio <rio@gmail.com>
2026-02-27 13:41:42 +0530

    modify: dressing

--- nvim/lua/plugins/dressing.lua
+++ nvim/lua/plugins/dressing.lua
@@ -1,16 +1,17 @@
 return {
 	"stevearc/dressing.nvim",
-	lazy = true,
-	init = function()
-		---@diagnostic disable-next-line: duplicate-set-field
-		vim.ui.select = function(...)
-			require("lazy").load({ plugins = { "dressing.nvim" } })
-			return vim.ui.select(...)
-		end
-		---@diagnostic disable-next-line: duplicate-set-field
-		vim.ui.input = function(...)
-			require("lazy").load({ plugins = { "dressing.nvim" } })
-			return vim.ui.input(...)
-		end
-	end,
+	event = "VeryLazy",
+	opts = {
+		input = {
+			enabled = true,
+			default_prompt = "➤ ",
+			win_opts = {
+				border = "rounded",
+				winhighlight = "Normal:Normal,FloatBorder:FloatBorder",
+			},
+		},
+		select = {
+			enabled = false,
+		},
+	},
 }
