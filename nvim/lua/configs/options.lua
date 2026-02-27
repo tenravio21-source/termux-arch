37c3fd1cf35d24574cb4392150e8ca8974fce5f0
rio <rio@gmail.com>
2026-02-27 11:44:37 +0530

    remove devicons

--- nvim/lua/configs/options.lua
+++ nvim/lua/configs/options.lua
@@ -118,5 +118,10 @@ local disabled_built_ins = {
 	"matchit",
 }
 for _, plugin in ipairs(disabled_built_ins) do
-	vim.g["loaded_" .. plugin] = 1
+	g["loaded_" .. plugin] = 1
 end
+
+g.loaded_python3_provider = 0
+g.loaded_ruby_provider = 0
+g.loaded_node_provider = 0
+g.loaded_perl_provider = 0
