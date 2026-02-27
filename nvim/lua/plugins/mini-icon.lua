e218e3c25771783858ada8b42ae28572cdbb6b33
rio <rio@gmail.com>
2026-02-27 14:52:46 +0530

    modify: mini

--- nvim/lua/plugins/mini-icon.lua
+++ nvim/lua/plugins/mini-icon.lua
@@ -1,25 +1,20 @@
 return {
-	"echasnovski/mini.icons",
-	lazy = true,
-	opts = {
-		file = {
-			[".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
-			["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
-		},
-		filetype = {
-			dotenv = { glyph = "", hl = "MiniIconsYellow" },
-		},
-	},
-
-	init = function()
-		package.preload["nvim-web-devicons"] = function()
-			local icons = require("mini.icons")
-			icons.mock_nvim_web_devicons()
-			return package.loaded["nvim-web-devicons"]
-		end
-	end,
-
-	config = function(_, opts)
-		require("mini.icons").setup(opts)
-	end,
+  "echasnovski/mini.icons",
+  lazy = true,
+  opts = {
+    file = {
+      [".keep"] = { glyph = "󰊢", hl = "MiniIconsGrey" },
+      ["devcontainer.json"] = { glyph = "", hl = "MiniIconsAzure" },
+    },
+    filetype = {
+      dotenv = { glyph = "", hl = "MiniIconsYellow" },
+    },
+  },
+  init = function()
+    -- Faster mocking: Register the provider but wait for the first request to load it
+    package.preload["nvim-web-devicons"] = function()
+      require("mini.icons").mock_nvim_web_devicons()
+      return package.loaded["nvim-web-devicons"]
+    end
+  end,
 }
