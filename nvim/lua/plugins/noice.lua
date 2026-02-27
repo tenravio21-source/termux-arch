3913448b3d8f060d0f68f51b2a449f0f825989a6
rio <rio@gmail.com>
2026-02-27 15:02:52 +0530

    modify: noice

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,18 +1,49 @@
 return {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	cmd = {
-		"Noice",
-		"NoiceEnable",
-		"NoiceDisable",
-		"NoiceLog",
-	},
-	dependencies = {
-		"MunifTanjim/nui.nvim",
-		"rcarriga/nvim-notify",
-	},
-	opts = {},
-	config = function()
-		require("core.noice").setup()
-	end,
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    cmdline = {
+      view = "cmdline_popup",
+      opts = { position = { row = 5, col = "50%" } },
+      format = {
+        cmdline = { pattern = "^:", icon = "", lang = "vim" },
+        search_down = { pattern = "^/", icon = " ", lang = "regex" },
+        search_up = { pattern = "^%?", icon = " ", lang = "regex" },
+        lua = { pattern = "^:%s*lua%s+", icon = "", lang = "lua" },
+        help = { pattern = "^:%s*he?l?p?%s+", icon = "󰋖" },
+      },
+    },
+    routes = {
+      { -- Skip "written" and undo/redo messages to keep the UI clean
+        filter = {
+          event = "msg_show",
+          any = { { find = "written" }, { find = "%d+L, %d+B" }, { find = "; after #%d+" } },
+        },
+        opts = { skip = true },
+      },
+    },
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+      hover = { silent = true },
+    },
+    presets = {
+      bottom_search = false,
+      command_palette = true,
+      long_message_to_split = true,
+      inc_rename = true,
+      lsp_doc_border = true,
+    },
+    views = {
+      cmdline_popup = { border = { style = "rounded" } },
+      popupmenu = { border = { style = "rounded" } },
+    },
+  },
 }
