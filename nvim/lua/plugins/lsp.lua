c66c74e3862980abbca2d6e1281fe44a21843803
rio <rio@gmail.com>
2026-02-27 17:18:38 +0530

    modify: lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,78 +1,78 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    cmd = "Mason",
-    opts = {
-      ui = { border = "rounded" },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-      "williamboman/mason-lspconfig.nvim",
-      "saghen/blink.cmp",
-    },
-    opts = {
-      servers = {
-        lua_ls = {
-          settings = {
-            Lua = {
-              runtime = { version = "LuaJIT" },
-              diagnostics = { globals = { "vim" } },
-              workspace = { 
-                checkThirdParty = false,
-                library = vim.api.nvim_get_runtime_file("", true),
-              },
-            },
-          },
-        },
-        pyright = {},
-        rust_analyzer = {},
-        gopls = {},
-        dartls = {},
-      },
-    },
-    config = function(_, opts)
-      require("mason-lspconfig").setup({
-        ensure_installed = vim.tbl_keys(opts.servers),
-      })
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		opts = {
+			ui = { border = "rounded" },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason-lspconfig.nvim",
+			"saghen/blink.cmp",
+		},
+		opts = {
+			servers = {
+				lua_ls = {
+					settings = {
+						Lua = {
+							runtime = { version = "LuaJIT" },
+							diagnostics = { globals = { "vim" } },
+							workspace = {
+								checkThirdParty = false,
+								library = vim.api.nvim_get_runtime_file("", true),
+							},
+						},
+					},
+				},
+				pyrefly = {},
+				rust_analyzer = {},
+				gopls = {},
+				dartls = {},
+			},
+		},
+		config = function(_, opts)
+			require("mason-lspconfig").setup({
+				ensure_installed = vim.tbl_keys(opts.servers),
+			})
 
-      local icons = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = "󰋽 " }
-      vim.diagnostic.config({
-        virtual_text = false,
-        severity_sort = true,
-        float = { border = "rounded", source = "if_many" },
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = icons.Error,
-            [vim.diagnostic.severity.WARN] = icons.Warn,
-            [vim.diagnostic.severity.HINT] = icons.Hint,
-            [vim.diagnostic.severity.INFO] = icons.Info,
-          },
-        },
-      })
+			local icons = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = "󰋽 " }
+			vim.diagnostic.config({
+				virtual_text = false,
+				severity_sort = true,
+				float = { border = "rounded", source = "if_many" },
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = icons.Error,
+						[vim.diagnostic.severity.WARN] = icons.Warn,
+						[vim.diagnostic.severity.HINT] = icons.Hint,
+						[vim.diagnostic.severity.INFO] = icons.Info,
+					},
+				},
+			})
 
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          local bufnr = args.buf
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					local bufnr = args.buf
 
-          local map = function(mode, lhs, rhs, desc)
-            vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = "LSP: " .. desc })
-          end
+					local map = function(mode, lhs, rhs, desc)
+						vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = "LSP: " .. desc })
+					end
 
-          if client and client.supports_method("textDocument/inlayHint") then
-            vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-          end
-        end,
-      })
-      local blink = require("blink.cmp")
-      for server, server_opts in pairs(opts.servers) do
-        server_opts.capabilities = blink.get_lsp_capabilities(server_opts.capabilities)
-        vim.lsp.config(server, server_opts)
-        vim.lsp.enable(server)
-      end
-    end,
-  },
+					if client and client.supports_method("textDocument/inlayHint") then
+						vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+					end
+				end,
+			})
+			local blink = require("blink.cmp")
+			for server, server_opts in pairs(opts.servers) do
+				server_opts.capabilities = blink.get_lsp_capabilities(server_opts.capabilities)
+				vim.lsp.config(server, server_opts)
+				vim.lsp.enable(server)
+			end
+		end,
+	},
 }
