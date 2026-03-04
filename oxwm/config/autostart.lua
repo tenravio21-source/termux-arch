a7e38909a1cedac25e7760c4bfcb18c613816e46
rio <rio@gmail.com>
2026-03-04 12:47:03 +0530

    Remove: oxwm

--- oxwm/config/autostart.lua
+++
@@ -1,5 +0,0 @@
----@module 'oxwm'
-
-oxwm.autostart("dunst")
--- oxwm.autostart("nm-applet")
-oxwm.autostart("xwallpaper --zoom ~/walls/landscape.png")
--- oxwm/config/rules.lua
+++
@@ -1,7 +0,0 @@
----@module 'oxwm'
-
-oxwm.rule.add({ instance = "gimp", floating = true })
-oxwm.rule.add({ instance = "brave-browser", tag = 2 })
-oxwm.rule.add({ class = "firefox", tag = 3 })
-oxwm.rule.add({ instance = "slack", tag = 4 })
-oxwm.rule.add({ instance = "discord", tag = 5 })
