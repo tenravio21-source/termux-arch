a1be48fbf951fcfbcadc747e0e0eb6a4d7169e2b
rio <rio@gmail.com>
2026-03-05 14:25:53 +0530

    Nvchad

--- NvChad/lua/chadrc.lua
+++ NvChad/lua/chadrc.lua
@@ -1,6 +1,6 @@
--- This file needs to have same structure as nvconfig.lua 
+-- This file needs to have same structure as nvconfig.lua
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
+-- Please read that file to know all available options :(
 
 ---@type ChadrcConfig
 local M = {}
@@ -16,9 +16,14 @@ M.base46 = {
 
 M.nvdash = { load_on_startup = true }
 M.ui = {
-      tabufline = {
-         lazyload = false
-     }
+	statusline = {
+		separator_style = "block", --  default, round, block, arrow
+		theme = "minimal",
+	},
+	--  default, vscode, vscode_colored, minimal
+	tabufline = {
+		lazyload = false,
+	},
 }
 
 return M
