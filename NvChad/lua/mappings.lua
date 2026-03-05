13e01587d6a47c1ba4789f354538f704cf45ea37
rio <rio@gmail.com>
2026-03-05 16:22:55 +0530

    nv

--- NvChad/lua/mappings.lua
+++ NvChad/lua/mappings.lua
@@ -1,14 +1,11 @@
 require("nvchad.mappings")
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
 -- In Terminal mode ('t'), double Esc will toggle (hide) the floating terminal
 map("t", "<Esc><Esc>", function()
 	require("nvchad.term").toggle({ pos = "float", id = "floatTerm" })
