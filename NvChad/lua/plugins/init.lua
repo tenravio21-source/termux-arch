158bfd0ab634a2d13748e0db72e8306bef3addb4
rio <rio@gmail.com>
2026-03-05 18:28:06 +0530

    nv

--- NvChad/lua/plugins/init.lua
+++ NvChad/lua/plugins/init.lua
@@ -1,28 +1,58 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		event = "BufWritePre", -- uncomment for format on save
+		opts = require("configs.conform"),
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
+	-- These are some examples, uncomment them if you want to see them work!
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
+	{
+		"nvim-telescope/telescope.nvim",
+		opts = {
+			defaults = {
+				initial_mode = "insert",
+				prompt_prefix = "   ",
+				selection_caret = "  ",
+				path_display = { "truncate" },
+				sorting_strategy = "ascending",
+				layout_config = {
+					horizontal = {
+						prompt_position = "top",
+						preview_width = 0.70,
+					},
+					width = 0.87,
+					height = 0.80,
+				},
+				file_ignore_patterns = {
+					"node_modules",
+					".git/",
+					".DS_Store",
+					"dist/",
+					"build/",
+				},
+			},
+			pickers = {
+				buffers = { sort_lastused = true },
+			},
+		},
+	},
 
-  -- test new blink
-  { import = "nvchad.blink.lazyspec" },
+	-- test new blink
+	{ import = "nvchad.blink.lazyspec" },
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	opts = {
+	-- 		ensure_installed = {
+	-- 			"vim", "lua", "vimdoc",
+	--      "html", "css"
+	-- 		},
+	-- 	},
+	-- },
 }
