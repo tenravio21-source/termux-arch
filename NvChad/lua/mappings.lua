66a6b291f1e84afebdd6ce1d91d65e4192420794
rio <rio@gmail.com>
2026-03-05 16:21:27 +0530

    nv

--- NvChad/lua/mappings.lua
+++ NvChad/lua/mappings.lua
@@ -13,3 +13,9 @@ map("i", "jk", "<ESC>")
 map("t", "<Esc><Esc>", function()
 	require("nvchad.term").toggle({ pos = "float", id = "floatTerm" })
 end, { desc = "Terminal Toggle floating term" })
+
+-- Resize window using leader + arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
