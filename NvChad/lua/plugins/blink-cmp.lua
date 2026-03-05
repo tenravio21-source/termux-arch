9000a50ce0db9968808bd76a3ae8a0449d6fed5c
rio <rio@gmail.com>
2026-03-05 23:54:57 +0530

    blink-cmp

---
+++ NvChad/lua/plugins/blink-cmp.lua
@@ -0,0 +1,97 @@
+return {
+	"saghen/blink.cmp",
+	version = "v0.*",
+	dependencies = {
+		{ "saghen/blink.compat", opts = { enable_events = true } },
+		{
+			"Exafunction/codeium.nvim",
+			cmd = "Codeium", -- Lazy load on command
+			build = ":Codeium Auth",
+			opts = {},
+		},
+		{
+			"L3MON4D3/LuaSnip",
+			build = "make install_jsregexp",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load({
+					paths = vim.fn.stdpath("config") .. "/snippets",
+				})
+			end,
+		},
+	},
+	opts = {
+		-- Use your custom keymaps
+		keymap = {
+			["<C-k>"] = { "fallback" },
+			["<C-b>"] = { "fallback" },
+			["<C-z>"] = { "select_and_accept" },
+			["<Tab>"] = {
+				function(cmp)
+					if cmp.is_visible() then
+						return cmp.select_next()
+					end
+					if cmp.snippet_active() then
+						return cmp.snippet_forward()
+					end
+					return false
+				end,
+				"fallback",
+			},
+			["<S-Tab>"] = {
+				function(cmp)
+					if cmp.is_visible() then
+						return cmp.select_prev()
+					end
+					if cmp.snippet_active() then
+						return cmp.snippet_backward()
+					end
+					return false
+				end,
+				"fallback",
+			},
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<C-f>"] = { "scroll_documentation_up", "fallback" },
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+		},
+
+		snippets = {
+			preset = "luasnip",
+		},
+
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			kind_icons = {
+				Codeium = "",
+				-- ... (rest of your icons)
+			},
+		},
+
+		sources = {
+			default = { "lsp", "codeium", "path", "snippets", "buffer" },
+			providers = {
+				codeium = {
+					name = "codeium",
+					module = "blink.compat.source",
+					score_offset = 100,
+					async = true,
+					transform_items = function(_, items)
+						for _, item in ipairs(items) do
+							item.kind = vim.lsp.protocol.CompletionItemKind.Event
+							item.kind_name = "Codeium"
+						end
+						return items
+					end,
+				},
+			},
+		},
+
+		completion = {
+			ghost_text = { enabled = true },
+			menu = { border = "rounded" },
+			documentation = { auto_show = true, window = { border = "rounded" } },
+		},
+		signature = { enabled = true, window = { border = "rounded" } },
+	},
+}
