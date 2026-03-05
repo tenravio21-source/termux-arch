9ad790ff3a3f173dbf4e1a11c2823ad8011ca1fb
rio <rio@gmail.com>
2026-03-05 16:17:06 +0530

    nv

--- NvChad/lua/configs/conform.lua
+++ NvChad/lua/configs/conform.lua
@@ -1,16 +1,21 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    rust = {"rust_analyzer"}
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		rust = { "rust_analyzer" },
+		dart = { "dartls" },
+		-- c = { "clang-format" },
+		-- cpp = { "clang-format" },
+		-- go = { "gofumpt", "goimports-reviser", "golines" },
+		-- css = { "prettier" },
+		-- html = { "prettier" },
+		-- python = { "isort", "black" },
+	},
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
 }
 
 return options
