062d892fa3cd187953f16a321c2d8a70549bde76
rio <rio@gmail.com>
2026-03-06 15:38:45 +0530

    blink

--- NvChad/lua/plugins/blink-cmp.lua
+++ NvChad/lua/plugins/blink-cmp.lua
@@ -110,11 +110,52 @@ return {
 				},
 			},
 		},
-
+		cmdline = {
+			keymap = {
+				["<Tab>"] = { "show", "accept" },
+			},
+			completion = {
+				menu = {
+					auto_show = true,
+					draw = {
+						columns = {
+							{ "label", "label_description", gap = 1 },
+							{ "kind_icon", "kind", gap = 1 },
+						},
+					},
+				},
+			},
+		},
 		completion = {
-			ghost_text = { enabled = true },
-			menu = { border = "rounded" },
-			documentation = { auto_show = true, window = { border = "rounded" } },
+			trigger = {
+				prefetch_on_insert = false,
+				show_on_trigger_character = true,
+			},
+			ghost_text = { enabled = true, show_without_selection = true },
+			menu = {
+				border = "rounded",
+				draw = {
+					columns = {
+						{ "label", "label_description", gap = 1 },
+						{ "kind_icon", "kind", gap = 1 },
+					},
+					components = {
+						kind_icon = {
+							ellipsis = false,
+							text = function(ctx)
+								return ctx.kind_icon .. ctx.icon_gap
+							end,
+							highlight = function(ctx)
+								if ctx.item.source_name == "codeium" then
+									return "BlinkCmpKindCodeium"
+								end
+								return "BlinkCmpKind" .. ctx.kind
+							end,
+						},
+					},
+				},
+			},
+			documentation = { auto_show = true, auto_show_delay_ms = 200, window = { border = "rounded" } },
 		},
 		signature = { enabled = true, window = { border = "rounded" } },
 	},
